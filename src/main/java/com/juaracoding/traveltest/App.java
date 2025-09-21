eb041667acc5a1b522581be1af64bb08e60fb101
muhseptian1402 <muh.septian.1402@gmail.com>
2025-09-21 18:18:18 +0700

    first commit

---
+++ src/main/java/com/juaracoding/traveltest/App.java
@@ -0,0 +1,15 @@
+package com.juaracoding.traveltest;
+
+
+
+/**
+ * Hello world!
+ *
+ */ 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/test/java/com/juaracoding/traveltest/AppTest.java
@@ -0,0 +1,97 @@
+package com.juaracoding.traveltest;
+
+import java.util.List;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class AppTest     {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+    }
+
+    @Test
+    public void registerUserTest()  {
+        WebElement firstnameField = driver.findElement(By.name("firstName"));
+        WebElement lastnameField = driver.findElement(By.name("lastName"));
+        WebElement phoneField = driver.findElement(By.name("phone"));
+        WebElement emailField = driver.findElement(By.name("userName"));
+        WebElement addressField = driver.findElement(By.name("address1"));
+        WebElement cityField = driver.findElement(By.name("city"));
+        WebElement stateField = driver.findElement(By.name("state"));
+        WebElement postalCodeField = driver.findElement(By.name("postalCode"));
+        
+        firstnameField.sendKeys("Septian");
+        
+        lastnameField.sendKeys("Firmansyah");
+        phoneField.sendKeys("08123123456");
+        emailField.sendKeys("septian.123@gmail.com");
+        addressField.sendKeys("Jl.Sesama No.123");
+        cityField.sendKeys("Konoha");
+        stateField.sendKeys("East Konoha");
+        postalCodeField.sendKeys("123442");
+        
+        
+        
+        
+        
+    }
+    // Kalau ingin melakukan pengujian secara random country di setiap pengujiannya
+    @Test
+    public void selectRandomCountryTest(){
+        WebElement countryField = driver.findElement(By.name("country"));
+        Select select = new Select(countryField);
+       
+        List<WebElement> options = select.getOptions();
+        
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(options.size());
+
+        select.selectByIndex(randomIndex);
+
+        WebElement selectedOption = select.getFirstSelectedOption();
+
+        Assert.assertEquals(selectedOption.getText(), options.get(randomIndex).getText()); 
+        
+    }
+
+    @Test
+    public void userInformationTest(){
+        WebElement usernameField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement confirmPassField = driver.findElement(By.name("confirmPassword"));
+        WebElement button = driver.findElement(By.name("submit"));
+        
+        usernameField.sendKeys("Septian123");
+        passwordField.sendKeys("Septian123_");
+        confirmPassField.sendKeys("Septian123_");
+
+        button.click();
+
+        String actual = driver.getCurrentUrl();
+        String expected = "https://demo.guru99.com/test/newtours/register_sucess.php";
+
+        Assert.assertEquals(actual, expected);
+    
+    }
+
+    @AfterClass   
+    public void teardown() throws InterruptedException {
+        Thread.sleep(4000);
+        driver.close();
+    }
+    
+}
